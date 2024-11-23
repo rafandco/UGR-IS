dd5735e5f658990b4c58704c7db7a4ef6d8132f7
Rafael Díaz <rafandpufff@gmail.com>
2024-11-23 19:40:28 +0100

    Cambiar de recurso a tarea

--- flask_service_crud/crud.py
+++ flask_service_crud/crud.py
@@ -1,74 +1,76 @@
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-info = [
-    {'id': 1, 'nombre': 'Recurso 1'},
-    {'id': 2, 'nombre': 'Recurso 2'},
-    {'id': 3, 'nombre': 'Recurso 3'}
+tareas = [
+    {'id': 1, 'nombre': 'Recoger habitación'},
+    {'id': 2, 'nombre': 'Formatear PC'},
+    {'id': 3, 'nombre': 'Hacer la compra'}
 ]
 
-def obtener_recurso_por_id(recurso_id):
-    for recurso in info:
-        if recurso['id'] == int(recurso_id):
-            return recurso
+def obtener_tarea_por_id(tarea_id):
+    for tarea in tareas:
+        if tarea['id'] == int(tarea_id):
+            return tarea
     return None
 
-def actualizar_recurso_por_id(recurso_id, data):
-    for recurso in info:
-        if recurso['id'] == int(recurso_id):
-            recurso['nombre'] = data['nombre']
+def actualizar_tarea_por_id(tarea_id, data):
+    for tarea in tareas:
+        if tarea['id'] == int(tarea_id):
+            tarea['nombre'] = data['nombre']
             return True
     return False
 
-def eliminar_recurso_por_id(recurso_id):
-    for recurso in info:
-        if recurso['id'] == int(recurso_id):    
-            info.remove(recurso)
+def eliminar_tarea_por_id(tarea_id):
+    for tarea in tareas:
+        if tarea['id'] == int(tarea_id):    
+            tareas.remove(tarea)
             return True
     return False
-@app.route('/recuperar/<recurso_id>', methods=['GET'])
-def obtener_recurso(recurso_id):
-# Aquí puedes acceder al ID del recurso desde la URL
-# Recupera el recurso con el ID proporcionado
-    recurso = obtener_recurso_por_id(recurso_id)
-    if recurso is not None:
-# Devuelve el recurso encontrado, por ejemplo, en formato JSON
-        return jsonify(recurso)
+
+# Rutas para las acciones CRUD    
+@app.route('/tareas/<tarea_id>', methods=['GET'])
+def obtener_tarea(tarea_id):
+    # Aquí puedes acceder al ID de la tarea desde la URL
+    # Recupera la tarea con el ID proporcionado
+    tarea = obtener_tarea_por_id(tarea_id)
+    if tarea is not None:
+    # Devuelve la tarea encontrado, por ejemplo, en formato JSON
+        return jsonify(tarea)
     else:
-# Devuelve una respuesta de error si el recurso no se encuentra
+    # Devuelve una respuesta de error si la tarea no se encuentra
         return jsonify({'error': 'Recurso no encontrado'}), 404
 
-@app.route('/crear', methods=['POST'])
-def crear_recurso():
+@app.route('/tareas', methods=['POST'])
+def crear_tarea():
     # Aquí puedes acceder a los datos enviados por el cliente a través de request
     data = request.json # Suponiendo que los datos se envían en formato JSON
     # Procesa los datos y crea el nuevo recurso
-    info.append({'id': data['id'], 'nombre': data['nombre']})
+    tareas.append({'id': data['id'], 'nombre': data['nombre']})
     # Devuelve una respuesta adecuada, por ejemplo, un código 201 (Created)
-    return jsonify({'mensaje': 'Recurso creado correctamente',
+    return jsonify({'mensaje': 'Tarea creada correctamente',
                     "data": data}), 201
 
-@app.route('/actualizar/<recurso_id>', methods=['PUT'])
-def actualizar_recurso(recurso_id):
-    # Aquí puedes acceder al ID del recurso desde la URL
+@app.route('/tareas/<tarea_id>', methods=['PUT'])
+def actualizar_tarea(tarea_id):
+    # Aquí puedes acceder al ID de la tarea desde la URL
     # Accede a los datos enviados por el cliente a través de request
     data = request.json # Suponiendo que los datos se envían en formato JSON
-    # Actualiza el recurso con el ID proporcionado
-    actualizado = actualizar_recurso_por_id(recurso_id, data)
+    # Actualiza la tarea con el ID proporcionado
+    actualizado = actualizar_tarea_por_id(tarea_id, data)
     if actualizado:
-        return jsonify({'mensaje': 'Recurso actualizado correctamente'})
+        return jsonify({'mensaje': 'Tarea actualizada correctamente'})
     else:
-        return jsonify({'error': 'Recurso no encontrado'}), 404
+        return jsonify({'error': 'Tarea no encontrada'}), 404
 
-@app.route('/eliminar/<recurso_id>', methods=['DELETE'])
-def eliminar_recurso(recurso_id):
-    # Aquí puedes acceder al ID del recurso desde la URL
-    # Elimina el recurso con el ID proporcionado
-    eliminado = eliminar_recurso_por_id(recurso_id)
+@app.route('/tareas/<tarea_id>', methods=['DELETE'])
+def eliminar_tarea(tarea_id):
+    # Aquí puedes acceder al ID de la tarea desde la URL
+    # Elimina la tarea con el ID proporcionado
+    eliminado = eliminar_tarea_por_id(tarea_id)
     if eliminado:
-        return jsonify({'mensaje': 'Recurso eliminado correctamente'})
+        return jsonify({'mensaje': 'Tarea eliminada correctamente'})
     else:
-        return jsonify({'error': 'Recurso no encontrado'}), 404
+        return jsonify({'error': 'Tarea no encontrada'}), 404
     
 if __name__ == '__main__':
     app.run()
